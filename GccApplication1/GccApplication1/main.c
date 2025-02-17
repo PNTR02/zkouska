841b88141dd1d2fbb1e831860ace0645cb6ad0b8
Radim Vancl <vanclradim@seznam.cz>
2025-02-17 20:29:55 +0100

    pridani souboru

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 17.02.2025 20:23:16
+ * Author : Radimek
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}	
+
